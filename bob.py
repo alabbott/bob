138c5a1ebb0d2d5dd480c3cd45fa0f5847a158c3
Alan Abbott <alabbott@pm.me>
2022-12-05 19:48:40 -1000

    added functions

--- bob.py
+++ bob.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
 import openai
-
-# test
+import psycopg2
 
 load_dotenv()
 openai.api_key = os.environ.get('OPENAI_KEY')
@@ -27,3 +26,54 @@ def append_interaction_to_chat_log(question, answer, chat_log=None):
     if chat_log is None:
         chat_log = start_chat_log
     return f'{chat_log}Human: {question}\nAI: {answer}\n'
+
+def classifyRequest(message):
+    prompt = f'Classify the following sentence into these categories: eating food, current weight, questions, other. "[{message}]"'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
+
+def handleMessage(message, category):
+    if category == "eating food":
+        # handle eating food
+        return message
+    elif category == "current weight":
+        # handle
+        return message
+    elif category == "current weight":
+        # handle
+        return message
+    else:
+        # handle
+        return message
+
+def executeQuery(query):
+    # Connect to the database
+    conn = psycopg2.connect(
+        host="localhost",
+        database="my_database",
+        user="postgres",
+        password='POSTGRES_PASS'
+    )
+
+    # Create a cursor to interact with the database
+    cur = conn.cursor()
+
+    # Execute a SQL query
+    cur.execute(query)
+
+    # Fetch the query results
+    results = cur.fetchall()
+
+    # Loop through the results and print each row
+    # for row in results:
+    #    print(row)
+
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
+
+    return results
